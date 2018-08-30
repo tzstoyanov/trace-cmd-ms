f0f75f1d85f2d9e9f3d7a37e0d2ddd446b61b019
Yordan Karadzhov (VMware) <y.karadz@gmail.com>
2018-08-30 22:31:53 +0300

    kernel-shark-qt: Add kshark-record executable
    kshark-record provides a simple GUI (dialog) used to generate command line
    arguments for trace-cmd and to record trace data.
    
    Signed-off-by: Yordan Karadzhov (VMware) <y.karadz@gmail.com>

---
+++ kernel-shark-qt/src/kshark-record.cpp
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+ * Copyright (C) 2018 VMware Inc, Yordan Karadzhov <y.karadz@gmail.com>
+ */
+
+ /**
+  *  @file    kshark-record.cpp
+  *  @brief   kshark-record executable.
+  */
+
+// C
+#include <unistd.h>
+
+// KernelShark
+#include "KsCaptureDialog.hpp"
+
+int main(int argc, char **argv)
+{
+	QApplication a(argc, argv);
+	KsCaptureDialog cd;
+
+	int c;
+	while ((c = getopt(argc, argv, "o:")) != -1) {
+		switch(c) {
+		case 'o':
+			cd.setOutputFileName(QString(optarg));
+			break;
+		}
+	}
+
+	cd.show();
+	return a.exec();
+}
